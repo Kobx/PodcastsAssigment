7460c8bb446f2746d12aebf4a81cd87f5300cd22
Kobx <Kobxxx@gmail.com>
2022-07-04 09:31:34 +0300

    Initial Commit

---
+++ PodcastsAssigment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcastsAssigment
+//
+//  Created by Koby Shirazi on 04/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
